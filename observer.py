871b2302a786c6f0204974e7dcb81077ede3c778
toripppppy <Lundy11@icloud.com>
2024-02-09 23:16:59 +0900

    memberの表示テキスト生成ロジックをUtilsに移動

--- observer.py
+++ observer.py
@@ -51,57 +51,11 @@ async def chat_ranking(message):
   embed = Embed.make_embed(description=text)
   await message.channel.send(embed = embed)
 
+# メンバー一覧を表示
 @bot.command(name="member")
 async def member(message):
-  max_string_length = 0 # 動的に[=]の文字数を変更するための変数
-  text = ""             # for文で作成するテキスト
-  output_text = ""      # 最終的に送信するテキスト
-  
-  for member in message.guild.members:
-    # Botだった場合
-    if member.bot: continue
-    
-    text += "・"
-    string_length = 2 # 最初に[・]の文字数分を足す
-    
-    # ニックネームがなかった場合
-    if member.nick == None:
-      text += member.name
-      string_length += Utils.check_width(member.name)
-    else:
-      text += member.name + "「" + member.nick + "」"
-      string_length += Utils.check_width(member.name + "「" + str(member.nick) + "」")
-    
-    # Role があったら記述する
-    role_list = [role.name for role in member.roles if role.name != "@everyone"]
-    role_text = ""
-    if len(role_list) != 0:
-      role_text += "［"
-      role_text += "  ".join(role_list)
-      role_text += "］\n"
-    else:
-      role_text += "\n"
-    
-    text += role_text
-    string_length += Utils.check_width(role_text)
-    
-    # 最大文字数を更新
-    max_string_length = max(max_string_length, string_length)
-
-  # 最大文字数が奇数だった場合偶数にする
-  # これをしないと[=]の文字数が合わなくなる
-  if max_string_length % 2:
-    max_string_length += 1
-  
-  # [=]の文字数を計算
-  # -14 は " メンバー一覧 " の文字数
-  # // 2 は両端に [=] を付けるため
-  x = (max_string_length - 13) // 2
-  output_text += ("=" * x) + " メンバー一覧 " + ("=" * x) + "\n"
-  output_text += text
-  output_text += "=" * (max_string_length - 1)
-  
-  embed = Embed.make_embed(description=output_text)
+  text = Utils.create_member_list_text(message)
+  embed = Embed.make_embed(description=text)
   await message.channel.send(embed = embed)
 
 # アドミンコマンドのルーティング
--- others/Utils.py
+++ others/Utils.py
@@ -18,7 +18,9 @@ def return_level_up_cnt(level, chat, length):
 
 def create_data_text(data: Data):
   """
-  整形した表示データを返す
+  observer data(-all)
+  ---
+  整形した表示テキストを返す
   """
   text = str()
   text += f"========== {data.name} ==========\n"
@@ -29,3 +31,59 @@ def create_data_text(data: Data):
   text += "="* (20 + len(data.name))
 
   return text
+
+def create_member_list_text(message):
+  """
+  observer member
+  ---
+  メンバー一覧の表示テキストを返す
+  """
+  max_string_length = 0 # 動的に[=]の文字数を変更するための変数
+  text = ""             # for文で作成するテキスト
+  output_text = ""      # 最終的に送信するテキスト
+  
+  for member in message.guild.members:
+    # Botだった場合
+    if member.bot: continue
+    
+    text += "・"
+    string_length = 2 # 最初に[・]の文字数分を足す
+    
+    # ニックネームがなかった場合
+    if member.nick == None:
+      text += member.name
+      string_length += check_width(member.name)
+    else:
+      text += member.name + "「" + member.nick + "」"
+      string_length += check_width(member.name + "「" + str(member.nick) + "」")
+    
+    # Role があったら記述する
+    role_list = [role.name for role in member.roles if role.name != "@everyone"]
+    role_text = ""
+    if len(role_list) != 0:
+      role_text += "［"
+      role_text += "  ".join(role_list)
+      role_text += "］\n"
+    else:
+      role_text += "\n"
+    
+    text += role_text
+    string_length += check_width(role_text)
+    
+    # 最大文字数を更新
+    max_string_length = max(max_string_length, string_length)
+
+  # 最大文字数が奇数だった場合偶数にする
+  # これをしないと[=]の文字数が合わなくなる
+  if max_string_length % 2:
+    max_string_length += 1
+  
+  # [=]の文字数を計算
+  # -14 は " メンバー一覧 " の文字数
+  # // 2 は両端に [=] を付けるため
+  x = (max_string_length - 13) // 2
+  output_text += ("=" * x) + " メンバー一覧 " + ("=" * x) + "\n"
+  output_text += text
+  output_text += "=" * (max_string_length - 1)
+
+  return output_text
